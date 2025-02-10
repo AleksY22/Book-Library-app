23a8d1a217781a0690889dd62f35c0fe06e9494f
Aleks <i.aleksey@tut.by>
2025-02-11 00:01:33 +0300

    Initial React App

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Library App</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
