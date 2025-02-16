088f48da78149b1a897b73056d44078b6e9de1a1
Aleks <i.aleksey@tut.by>
2025-02-16 15:02:24 +0300

    Filter by author

--- frontend/src/components/bookFilter/BookFilter.js
+++ frontend/src/components/bookFilter/BookFilter.js
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
   setTitleFilter,
+  setAuthorFilter,
   selectTitleFilter,
+  selectAuthorFilter,
   resetFilters,
 } from "../../redux/slices/filterSlice";
 import "./BookFilter.css";
@@ -9,8 +11,13 @@ import "./BookFilter.css";
 function BookFilter() {
   const dispatch = useDispatch();
   const titleFilter = useSelector(selectTitleFilter);
+  const authorFilter = useSelector(selectAuthorFilter);
+
   const handleTitleFilterChange = (e) => {
     dispatch(setTitleFilter(e.target.value));
+  };
+  const handleAuthorFilterChange = (e) => {
+    dispatch(setAuthorFilter(e.target.value));
   };
   const handleResetFilters = () => {
     dispatch(resetFilters());
@@ -25,6 +32,14 @@ function BookFilter() {
             value={titleFilter}
             placeholder="Filter by title..."
             onChange={handleTitleFilterChange}
+          />
+        </div>
+        <div className="book__filter-group">
+          <input
+            type="text"
+            value={authorFilter}
+            placeholder="Filter by author..."
+            onChange={handleAuthorFilterChange}
           />
         </div>
         <button onClick={handleResetFilters}>Reset Filters</button>
